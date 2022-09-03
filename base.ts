170e9498fc3b9ea6d0b6cc79002d40b28493070d
Schrubitteflau <sansonantoine@outlook.fr>
2022-09-03 19:00:48 +0200

    temp

--- base.ts
+++ base.ts
@@ -3,12 +3,17 @@ declare const __internalSymbol: unique symbol;
 
 type PrimitiveType = number | string;
 
-type TypeIdentifier<ValueType extends PrimitiveType, Key extends string> = ValueType & {
+type TypeIdentifier<ValueType extends PrimitiveType, Key> = ValueType & {
     __key: Key;
+    __primitive: ValueType;
     [__internalSymbol]: true;
-}
+};
+
+type InferPrimitive<TID> = TID extends { __primitive: infer Primitive } ? Primitive : never;
+type InferKey<TID> = TID extends { __key: infer Key } ? Key : never;
 
 type RuntimeCheckCallback<ValueType extends PrimitiveType> = (value: ValueType) => boolean;
+type ErrorMessageCallback<ValueType extends PrimitiveType> = (value: ValueType) => string;
 
 interface TypeHelpers<ValueType extends PrimitiveType, Key extends string> {
     is: (value: ValueType) => value is TypeIdentifier<ValueType, Key>;
@@ -16,15 +21,15 @@ interface TypeHelpers<ValueType extends PrimitiveType, Key extends string> {
     convert: (value: ValueType) => TypeIdentifier<ValueType, Key>;
 }
 
-function createStrictType<ValueType extends PrimitiveType, Key extends string>(runtimeCheckFn: RuntimeCheckCallback<ValueType>): TypeHelpers<ValueType, Key>
+function rawMakeTypeHelpers<ValueType extends PrimitiveType, Key extends string>(runtimeCheckCb: RuntimeCheckCallback<ValueType>, errorMessageCb: ErrorMessageCallback<ValueType>): TypeHelpers<ValueType, Key>
 {
     function typeguardFn(value: ValueType): value is TypeIdentifier<ValueType, Key> {
-        return runtimeCheckFn(value);
+        return runtimeCheckCb(value);
     };
 
     function assertFn(value: ValueType): asserts value is TypeIdentifier<ValueType, Key> {
-        if (!runtimeCheckFn(value)) {
-            throw new Error("aaa");
+        if (!runtimeCheckCb(value)) {
+            throw new Error(errorMessageCb(value));
         }
     };
 
@@ -39,3 +44,21 @@ function createStrictType<ValueType extends PrimitiveType, Key extends string>(r
         convert: toFn
     };
 }
+
+type Percent = TypeIdentifier<number, "Percent">;
+
+function makeTypeHelpers<TID>()
+{
+    
+}
+
+
+function makeTypeHelpersFromTypeIdentifier<TID>(): [InferKey<TID>, InferPrimitive<TID>]
+{
+    return {} as any;
+}
+
+
+// example usage
+
+const aaa = makeTypeHelpersFromTypeIdentifier<Percent>();
--- numeric.ts
+++ numeric.ts
@@ -5,14 +5,12 @@ type PositiveInteger = NumericType<"PositiveInteger">;
 
 // ajouter l'option où on utilise PositiveInteger et ça infer la key et du coup on peut manipuler
 // des PositiveInteger au lieu de NumericType<"PositiveInteger"> !!
-function createNumericType<Key extends string>(runtimeCheckFn: RuntimeCheckCallback<number>): NumericHelpers<Key>
-{
-    return createStrictType<number, Key>(runtimeCheckFn);
-}
+
+const createNumericType: (typeof createStrictType)<number, ""> = createStrictType;
 
 const PositiveInteger: NumericHelpers<"PositiveInteger"> = createNumericType<"PositiveInteger">((value: number) => {
     return value > 0;
-});
+}, (val) => "NONONONONONO");
 
 const a: number = 1;
 if (PositiveInteger.is(a))
